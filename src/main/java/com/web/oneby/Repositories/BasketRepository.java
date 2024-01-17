6fd3d47adf78753817149e32022fad9e371123b6
imramo00 <b.ramis.2002@gmail.com>
2024-01-18 02:41:09 +0600

    Added cost to book!

---
+++ src/main/java/com/web/oneby/Repositories/BasketRepository.java
@@ -0,0 +1,9 @@
+package com.web.oneby.Repositories;
+
+import com.web.oneby.Models.Basket;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BasketRepository extends JpaRepository<Basket, Long> {
+}
