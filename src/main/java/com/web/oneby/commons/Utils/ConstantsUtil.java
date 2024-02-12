c4286dd28945c19a29c955bca24ce5b71a88e6f0
imramo00 <b.ramis.2002@gmail.com>
2024-02-13 02:15:55 +0600

    Rewrote structure of project!

--- src/main/java/com/web/oneby/commons/Utils/ConstantsUtil.java
+++ src/main/java/com/web/oneby/commons/Utils/ConstantsUtil.java
@@ -7,13 +7,13 @@ public class ConstantsUtil {
     public static String HOST;
 
     //ADMIN info
-    public static String ADMIN_NAME_KZ = "Әкімші";
-    public static String ADMIN_NAME_RU = "Администратор";
-    public static String ADMIN_NAME_EN = "Administrator";
-    public static String ADMIN_LASTNAME_KZ = "Әкімші";
-    public static String ADMIN_LASTNAME_RU = "Администратор";
-    public static String ADMIN_LASTNAME_EN = "Administrator";
-    public static String ADMIN_USERNAME = "admin_one_by";
-    public static String ADMIN_PASSWORD = "12345678";
-    public static String ADMIN_EMAIL = "admin@oneby.kz";
+    public static final String ADMIN_NAME_KZ = "Әкімші";
+    public static final String ADMIN_NAME_RU = "Администратор";
+    public static final String ADMIN_NAME_EN = "Administrator";
+    public static final String ADMIN_LASTNAME_KZ = "Әкімші";
+    public static final String ADMIN_LASTNAME_RU = "Администратор";
+    public static final String ADMIN_LASTNAME_EN = "Administrator";
+    public static final String ADMIN_USERNAME = "admin_one_by";
+    public static final String ADMIN_PASSWORD = "12345678";
+    public static final String ADMIN_EMAIL = "admin@oneby.kz";
 }
