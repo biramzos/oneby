43109534949f0ae17333454ec4aba50897cb85c0
imramo00 <b.ramis.2002@gmail.com>
2024-03-25 04:05:52 +0500

    commit

--- src/main/java/com/web/oneby/modules/books/Models/Book.java
+++ src/main/java/com/web/oneby/modules/books/Models/Book.java
@@ -1,12 +1,16 @@
 package com.web.oneby.modules.books.Models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.web.oneby.commons.Enums.Language;
 import com.web.oneby.commons.Models.OBFile;
 import com.web.oneby.modules.books.Enums.Genre;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Entity
 @Table(name = "books")
@@ -72,5 +76,59 @@ public class Book {
         this.genres = genres;
         this.image = image;
         this.file = file;
+    }
+
+    public String getTitle(int language) {
+        Map<String, String> titles = new HashMap<>() {{
+            put("titleKK", titleKK);
+            put("titleRU", titleRU);
+            put("titleEN", titleEN);
+        }};
+        return titles.get("title" + Language.getLanguageById(language).suffix());
+    }
+
+    public String getTitle(Language language) {
+        Map<String, String> titles = new HashMap<>() {{
+            put("titleKK", titleKK);
+            put("titleRU", titleRU);
+            put("titleEN", titleEN);
+        }};
+        return titles.get("title" + language.suffix());
+    }
+
+    public String getDescription(int language) {
+        Map<String, String> descriptions = new HashMap<>() {{
+            put("descriptionKK", descriptionKK);
+            put("descriptionRU", descriptionRU);
+            put("descriptionEN", descriptionEN);
+        }};
+        return descriptions.get("description" + Language.getLanguageById(language).suffix());
+    }
+
+    public String getDescription(Language language) {
+        Map<String, String> descriptions = new HashMap<>() {{
+            put("descriptionKK", descriptionKK);
+            put("descriptionRU", descriptionRU);
+            put("descriptionEN", descriptionEN);
+        }};
+        return descriptions.get("description" + language.suffix());
+    }
+
+    public String getAuthor(int language) {
+        Map<String, String> authors = new HashMap<>() {{
+            put("authorKK", authorKK);
+            put("authorRU", authorRU);
+            put("authorEN", authorEN);
+        }};
+        return authors.get("author" + Language.getLanguageById(language).suffix());
+    }
+
+    public String getAuthor(Language language) {
+        Map<String, String> authors = new HashMap<>() {{
+            put("authorKK", authorKK);
+            put("authorRU", authorRU);
+            put("authorEN", authorEN);
+        }};
+        return authors.get("author" + language.suffix());
     }
 }
